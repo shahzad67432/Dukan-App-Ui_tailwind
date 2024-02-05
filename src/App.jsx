c10b4d543a17d56b67bfd8b41d7fbff8913a7379
shahzad67432 <shaa1891640@gmail.com>
2024-02-05 13:19:54 +0500

    adding side bar from flowbite ui free components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
 import {RevneuCard} from './components/RevneuCard'
+import Sidebar from './components/Sidebar'
 
 
 function App() {
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-2 ml-96 mt-44'>
-      
-      <RevneuCard className={"bg-blue-700 text-white"} title={"Ammount"} Ammount={"98,321.20"} orderCount={"13"}/>
-      <RevneuCard title={"Pending"} Ammount={"128.20"} orderCount={""}/>
-      <RevneuCard title={"Transaction"} Ammount={"233421.20"} orderCount={"18"}/>
-      
+    <div>
+      <div>
+        <Sidebar/>
+      </div>
     </div>
   )
 }
